c7c5bda6485b6bec585b27789d97503b58edcab9
Zineng Tang <82334223+zinengtang@users.noreply.github.com>
2023-01-27 04:12:53 -0500

    Update pretrain_mae_vam.sh

--- scripts/pretrain_mae_vam.sh
+++ scripts/pretrain_mae_vam.sh
@@ -1,4 +1,3 @@
 python run.py with data_root='./dataset' gpus=[0,1,2,3] num_nodes=1 task_mae_vam \
 per_gpu_batchsize=4 val_check_interval=0.1 num_workers=12 warmup_steps=1000 \
-load_hub_path='TVLT.ckpt'
-# load_local_path='.'
+load_local_path='mae_pretrain_vit_base_full.pth' strict_load=False

7971bab6c4d1502df5d810387a20cdb3630906b8
Zineng Tang <82334223+zinengtang@users.noreply.github.com>
2023-01-27 04:04:54 -0500

    Update msrvtt_dataset.py

--- model/data/datasets/msrvtt_dataset.py
+++ model/data/datasets/msrvtt_dataset.py
@@ -13,7 +13,7 @@ from .base_video_dataset import BaseVideoDataset
 class MsrvttDataset(BaseVideoDataset):
     def __init__(self, *args, split="", **kwargs):
         self.split = split
-        self.cut = "full-val"
+        self.cut = "miech"
         self.keys = None
         
         super().__init__(*args, **kwargs,)
@@ -50,9 +50,30 @@ class MsrvttDataset(BaseVideoDataset):
 
         split_dir = os.path.join(self.metadata_dir, 'high-quality', 'structured-symlinks')
 
-        train_list_path = "train_list_jsfusion.txt"
-        test_list_path = "val_list_jsfusion.txt"
-        js_test_cap_idx_path = "jsfusion_val_caption_idx.pkl"
+        js_test_cap_idx_path = None
+        challenge_splits = {"val", "public_server_val", "public_server_test"}
+        if self.cut == "miech":
+            train_list_path = "train_list_miech.txt"
+            test_list_path = "test_list_miech.txt"
+        elif self.cut == "jsfusion":
+            train_list_path = "train_list_jsfusion.txt"
+            test_list_path = "val_list_jsfusion.txt"
+            js_test_cap_idx_path = "jsfusion_val_caption_idx.pkl"
+        elif self.cut in {"full-val", "full-test"}:
+            train_list_path = "train_list_full.txt"
+            if self.cut == "full-val":
+                test_list_path = "val_list_full.txt"
+            else:
+                test_list_path = "test_list_full.txt"
+        elif self.cut in challenge_splits:
+            train_list_path = "train_list.txt"
+            if self.cut == "val":
+                test_list_path = f"{self.cut}_list.txt"
+            else:
+                test_list_path = f"{self.cut}.txt"
+        else:
+            msg = "unrecognised MSRVTT split: {}"
+            raise ValueError(msg.format(self.cut))
 
         train_df = pd.read_csv(os.path.join(split_dir, train_list_path), names=['videoid'])
         test_df = pd.read_csv(os.path.join(split_dir, test_list_path), names=['videoid'])
